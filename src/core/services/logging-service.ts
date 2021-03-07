ca768262823964312a06bb4dbf53502f7fd0d13e
Isabel Maskrey <isabel@satyrn.dev>
2021-03-06 21:05:59 -0500

    ✨ Logging service automatically applies log level from config

--- src/core/services/logging-service.ts
+++ src/core/services/logging-service.ts
@@ -1,14 +1,20 @@
 import { injectable } from "inversify";
-import { ILoggingService, Logger } from "@satyrnidae/apdb-api"
+import { IConfigurationService, ILoggingService, Logger, ServiceIdentifiers } from "@satyrnidae/apdb-api"
+import { inject } from "inversify";
+import { IAppConfiguration } from "./configuration/app-configuration";
 
 const Loggers: Logger[] = [];
 
 @injectable()
 export class LoggingService implements ILoggingService {
+
+  constructor(@inject(ServiceIdentifiers.Configuration) private readonly configurationService: IConfigurationService<IAppConfiguration>) {}
+
   public getLogger(loggerId: string): Logger {
     let logger: Logger = Loggers.find(l => l.getId() === loggerId);
     if (!logger) {
       logger = new Logger(loggerId);
+      this.configurationService.get('logLevel').then(logLevel => logger.setLogLevel(logLevel));
       Loggers.push(logger);
     }
 
