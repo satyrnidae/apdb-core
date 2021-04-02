d57d6e3e00004c97ba9b4ebaf579659cad23d679
Isabel Maskrey <isabel@satyrn.dev>
2021-04-02 14:27:33 -0400

    ✨ Set user presence

--- src/core/module/events/ready-handler.ts
+++ src/core/module/events/ready-handler.ts
@@ -1,7 +1,8 @@
-import { EventHandler, ServiceIdentifiers, IClientService, IEventService, ILoggingService, lazyInject, Logger } from "@satyrnidae/apdb-api";
+import { EventHandler, ServiceIdentifiers, IClientService, IEventService, ILoggingService, lazyInject, Logger, IConfigurationService, IModuleService, Module } from "@satyrnidae/apdb-api";
 import { CoreMessageService } from "../services/core-message-service";
 import { Client } from "discord.js";
 import { forEachAsync } from "@satyrnidae/apdb-utils";
+import { IAppConfiguration } from "core/services/configuration/app-configuration";
 
 export class ReadyHandler extends EventHandler<'ready'> {
   @lazyInject(CoreMessageService)
@@ -16,6 +17,12 @@ export class ReadyHandler extends EventHandler<'ready'> {
   @lazyInject(ServiceIdentifiers.Logging)
   private readonly loggingService!: ILoggingService;
 
+  @lazyInject(ServiceIdentifiers.Configuration)
+  private readonly configurationService!: IConfigurationService<IAppConfiguration>;
+
+  @lazyInject(ServiceIdentifiers.Module)
+  private readonly moduleService!: IModuleService;
+
   constructor(moduleId: string) {
     super('ready', moduleId);
   }
@@ -23,9 +30,17 @@ export class ReadyHandler extends EventHandler<'ready'> {
   public async handler(): Promise<void> {
     const log: Logger = this.loggingService.getLogger('core');
     const client: Client = this.clientService.getClient();
+    const prefix: string = await this.configurationService.get('defaultPrefix');
     this.eventService.on('error', (e: Error) => log.error(e));
     this.eventService.on('warn', (w: string) => log.warn(w));
     this.eventService.on('debug', (i: string) => log.debug(i));
+
+    // Set presence for global help command
+    client.user.setActivity(`${prefix}help`, {type: 'LISTENING'});
+
+    // Set the core module thumbnail to match the logged-in user profile picture
+    const coreModule: Module = await this.moduleService.getModuleById(this.moduleId);
+    coreModule.moduleInfo.details.thumbnail = client.user.avatarURL();
 
     log.info(`Logged in as ${client.user.tag}`);
 
